1522b4689202f2e2d7f67e82910b645786ef559f
Adam <adamfriden00@gmail.com>
2025-10-03 15:42:43 +0200

    Change cpu stress add multithreading

--- tutorial/assets/app.py
+++ tutorial/assets/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from multiprocessing import Pool, cpu_count
 import math
 import time
 
@@ -9,15 +10,17 @@ app = Flask(__name__)
 def hello():
     return "Flask app is running!"
 
+def work(x):
+    return sum(math.sqrt(i)*i for i in range(1000000))
 
 @app.route("/cpu-stress")
 def cpu_stress():
     # Burn CPU cycles for ~5 seconds
-    end_time = time.time() + 5
-    while time.time() < end_time:
-        math.sqrt(12345 * 54321)
-    return "CPU stress test complete! Burned ~5s of CPU."
+    with Pool(cpu_count()) as p: 
+        results = p.map(work, range(cpu_count()))
 
+    total = sum(results)
+    return f"Result from cpu stress: {total}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
